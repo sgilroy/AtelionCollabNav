57cb2f3007e5230178d6f18ef91cb8d834bc1efb
Scott Gilroy <scottjgilroy@gmail.com>
2013-07-10 11:47:22 -0400

    Initial version

---
+++ src/com/atelionhealth/AtelionCollabNav/MyActivity.java
@@ -0,0 +1,17 @@
+package com.atelionhealth.AtelionCollabNav;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class MyActivity extends DroidGap
+{
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
